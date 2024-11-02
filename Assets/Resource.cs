4d0e1eb5eca4e6daff148a9f5353024c4ae85b16
rsiddi7777 <rsiddi@ufl.edu>
2024-11-02 00:26:20 -0400

    spooky game manager and scriptable objects

---
+++ Assets/Resource.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Resource", menuName = "Inventory/Resource")]
+public class Resource : ScriptableObject
+{
+    new public string name = "New Resource";
+    public Sprite icon = null;
+    public float burnTime = 100f;
+    
+}
