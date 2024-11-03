97da60b7d5dfa0b2ee7bbafbedd67419f51ed144
Quantomato <juansebastianlomanto@yahoo.com>
2024-11-03 10:40:37 -0500

    Finished Hearth Animations
    Hearth Animations are now fully functional, also, my dialogue UI got deleted again :(

--- Assets/Scripts/Hearth_Stats.cs
+++ Assets/Scripts/Hearth_Stats.cs
@@ -14,15 +14,17 @@ public class Hearth_Stats : MonoBehaviour
     public static Boolean hearthLife = true;
     //Variable for Village Mood: 0 = Dead, 1 = Critical, 2 = Waning, 3 = Healthy, 4 = Blazing (Bullshit?)
     public static int mood;
-    public GameObject weakFire;
-    public GameObject moderateFire;
-    public GameObject strongFire;
-    public GameObject strongestFire;
+    public Animator fireImage;
+    public Image barImage;
+    public AnimatorController weakFire;
+    public AnimatorController moderateFire;
+    public AnimatorController strongFire;
+    public AnimatorController strongestFire;
     public Image fadeImage;
-    public GameObject strongestBar;
-    public GameObject strongBar;
-    public GameObject moderateBar;
-    public GameObject weakBar;
+    public Sprite strongestBar;
+    public Sprite strongBar;
+    public Sprite moderateBar;
+    public Sprite weakBar;
     public CanvasGroup fadePanel;
     public GameObject endScreen;
     public TextMeshProUGUI messageText;
@@ -42,13 +44,14 @@ public class Hearth_Stats : MonoBehaviour
     void Start()
     {
         // Preps the stage for having all game objects be true
-        strongestFire.SetActive(true); strongFire.SetActive(false); moderateFire.SetActive(false); weakFire.SetActive(false);
+        fireImage.runtimeAnimatorController = strongestFire;
         mood = 4;
         StartCoroutine(CountDownHealth());
     }
 
     private void Update()
     {
+        Debug.Log(currentHealth);
         //Check if it's Dead
         if (currentHealth == 0)
         {
@@ -61,104 +64,30 @@ public class Hearth_Stats : MonoBehaviour
         //Check if it's critical
         else if (currentHealth <= 25)
         {
-            if (mood != 1)
-            {
-                strongestBar.SetActive(false);
-                strongBar.SetActive(false);
-                moderateBar.SetActive(false);
-                weakBar.SetActive(true);
-                if (mood == 4)
-                {
-                    switchObjects(strongestFire, weakFire);
-                }
-                else if (mood == 3)
-                {
-                    switchObjects(strongFire, weakFire);
-                }
-                else if (mood == 2)
-                {
-                    switchObjects(moderateFire, weakFire);
-                }
-                else { Debug.Log("Something wrong with dying fire Update() in Hearth_Stats"); }
-                mood = 1;
-            }
-
+            mood = 1;
+            fireImage.runtimeAnimatorController = weakFire;
+            barImage.sprite = weakBar;
         }
         //Check if it's Waning
         else if (currentHealth <= 50)
         {
-            strongestBar.SetActive(false);
-            strongBar.SetActive(false);
-            moderateBar.SetActive(true);
-            weakBar.SetActive(false);
-            if (mood != 2)
-            {
-                if (mood == 4)
-                {
-                    switchObjects(strongestFire, moderateFire);
-                }
-                else if (mood == 3)
-                {
-                    switchObjects(strongFire, moderateFire);
-                }
-                else if (mood == 1)
-                {
-                    switchObjects(weakFire, moderateFire);
-                }
-                else { Debug.Log("Something wrong with waning fire Update() in Hearth_Stats"); }
-                mood = 2;
-            }
+            mood = 2;
+            fireImage.runtimeAnimatorController = moderateFire;
+            barImage.sprite = moderateBar;
         }
         //Check if it's Healthy
         else if (currentHealth <= 75)
         {
-            strongestBar.SetActive(false);
-            strongBar.SetActive(true);
-            moderateBar.SetActive(false);
-            weakBar.SetActive(false);
-            if (mood != 3)
-            {
-                if (mood == 4)
-                {
-                    switchObjects(strongestFire, strongFire);
-                }
-                else if (mood == 2)
-                {
-                    switchObjects(moderateFire, strongFire);
-                }
-                else if (mood == 1)
-                {
-                    switchObjects(weakFire, strongFire);
-                }
-                else { Debug.Log("Something wrong with healthy fire Update() in Hearth_Stats"); }
-                mood = 3;
-            }
+            mood = 3;
+            fireImage.runtimeAnimatorController = strongFire;
+            barImage.sprite = strongBar;
         }
         //Check if it's (bullshit?) Blazing
         else if (currentHealth <= maxHealth)
         {
-            strongestBar.SetActive(true);
-            strongBar.SetActive(false);
-            moderateBar.SetActive(false);
-            weakBar.SetActive(false);
-            if (mood != 4) 
-            {
-                if (mood == 3)
-                {
-                    switchObjects(strongFire, strongestFire);
-
-                }
-                else if (mood == 2)
-                {
-                    switchObjects(moderateFire, strongestFire);
-                }
-                else if (mood == 1) 
-                {
-                    switchObjects(weakFire, strongestFire);
-                }
-                else { Debug.Log("Something wrong with blazing fire Update() in Hearth_Stats"); }
-                mood = 4;
-            }
+            mood = 4;
+            fireImage.runtimeAnimatorController = strongestFire;
+            barImage.sprite = strongestBar;
         }
     }
 
