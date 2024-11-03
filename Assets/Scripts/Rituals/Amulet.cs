c6561848c2bae396f40fc102e092eb3fa1639667
Bri <45642328+brichua@users.noreply.github.com>
2024-11-03 01:41:29 -0400

    dialogue fade

--- Assets/Scripts/Rituals/Amulet.cs
+++ Assets/Scripts/Rituals/Amulet.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,34 +7,58 @@ using TMPro;
 public class Amulet : Interactable
 {
     public CanvasGroup fadePanel;
+    public GameObject amulet;
     public TextMeshProUGUI messageText;
-    public string[] messages;
+
+    public string[] messages = {
+        "I told you, there was something wrong with the fire. Why is it that everytime I forget to feed it, everyone is losing their minds?",
+        "I-I don’t know honey. Our ancestors would not have let it burn on this long if something was wrong though.",
+        "I don’t know how to describe it. But the fire, it’s controlling us. It’s not the safe flame that we think it is, I just know it!",
+        "But the villagers will kill us if we destroy it, we just can’t.",
+        "But this fire, it’s killing them. It doesn’t protect us from the cold anymore, it only uses the cold to control us.",
+        "Wait, I think I figured it out.",
+        "You see your father rummage through a bookcase of historical records, until he finds a book on the Stone Gods.",
+        "Each of the records here indicate some sort of resolution to our God’s Stories, like how Wíčaglata took flight to the skies to defend against the Celestial Furnace. But I couldn’t help but notice that one story looked odd.",
+        "The story for Wenku Wepe, it’s borderline incomplete. There isn’t an incredible feat like the other Gods, just “set out for greater horizons.” It’s nonsense, we haven’t been able to discover anything about him.",
+        "I had my suspicions too. It must be them, I know it. But why aren’t there any records on them?",
+        "There must be a reason for that, I just know it. A lot of records are missing, this is why we know so little.",
+        "But what should we do? Let the fire burn out? What if we’re wrong?",
+        "We’re not wrong, I know it. The fire is already weakening, we just need to let it die naturally and then-",
+        "A dissonant voice appears to come from the fire. It sounds miserable almost.",
+        "Feeeeed meeeeee. Don’t let me dieeeeeee.",
+        "Pleeeeaseeeee.",
+        "Don’t listen to it, we have to go. I have a bad feeling about this.",
+        "FEEEEEEEED MEEEEEE.",
+        "Just as quick as you witness your parents turn around, they are swept into the fire by an overwhelming force. You see no signs of a struggle, no screams of agony. Nothing.",
+        "After what feels like an eternity of the fire blazing uncontrollably, it calms down to what you recognize to be in its healthy state.",
+        "Nobody was around to witness the “accident,” nobody saw what that monster did to your parents, to your only family.",
+        "Nobody knows where they went, but everyone guesses what happened. All you can see now is a girl with dark brown hair and a red styled outfit approach the fire and pick up the amulet of her mother, the amulet that she promised to give to her on the day she would inherit the job from her.",
+        "The third offering has been submitted…",
+        "…………………", "Or has it?"
+
+    };
+
+    public Color[] messageColors = {
+        Color.white, Color.yellow, Color.white, Color.yellow,
+        Color.white, Color.yellow, Color.grey, Color.white,
+        Color.white, Color.yellow, Color.white, Color.white,
+        Color.yellow, Color.grey, Color.red, Color.red, Color.white,
+        Color.red, Color.grey, Color.grey, Color.grey, Color.grey, Color.grey,
+        Color.grey, Color.grey
+    };
+
     public float fadeDuration = 1f;
-    public float messageDisplayTime = 2f;
 
     private bool playerInRange = false;
     public static bool isPickedUp = true;
     public static bool appeared = false;
+    private int count = 0;
 
 
     public override void Interact()
     {
         base.Interact();
         StartCoroutine(PickupSequence());
-    }
-
-    IEnumerator PickupSequence()
-    {
-        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 0, 1, fadeDuration));
-
-        foreach (string message in messages)
-        {
-            yield return StartCoroutine(ShowMessage(message));
-        }
-
-        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 1, 0, fadeDuration));
-
-        gameObject.SetActive(false);
     }
 
     IEnumerator FadeCanvasGroup(CanvasGroup cg, float startAlpha, float endAlpha, float duration)
@@ -52,12 +76,50 @@ public class Amulet : Interactable
         cg.blocksRaycasts = endAlpha == 1;
     }
 
+    IEnumerator PickupSequence()
+    {
+        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 0, 1, fadeDuration));
+        amulet.SetActive(false);
+        foreach (string message in messages)
+        {
+            yield return ShowMessage(message);
+        }
+
+        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 1, 0, fadeDuration));
+    }
+
     IEnumerator ShowMessage(string message)
     {
         messageText.text = message;
-        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 0, 1, fadeDuration));
-        yield return new WaitForSeconds(messageDisplayTime);
-        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 1, 0, fadeDuration));
+
+        Debug.Log(messageColors[count]);
+        messageText.color = messageColors[count];
+
+        yield return StartCoroutine(FadeTextAlpha(messageText, 0, 1, fadeDuration));
+        yield return new WaitForSeconds(3f);
+        yield return StartCoroutine(FadeTextAlpha(messageText, 1, 0, fadeDuration));
+
+        yield return new WaitForSeconds(0.5f);
+        count++;
+    }
+
+    IEnumerator FadeTextAlpha(TextMeshProUGUI text, float startAlpha, float endAlpha, float duration)
+    {
+        Color color = text.color;
+        color.a = startAlpha;
+        text.color = color;
+
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            color.a = Mathf.Lerp(startAlpha, endAlpha, elapsed / duration);
+            text.color = color;
+            yield return null;
+        }
+
+        color.a = endAlpha;
+        text.color = color;
     }
 
 }
--- Assets/Scripts/Rituals/Coin.cs
+++ Assets/Scripts/Rituals/Coin.cs
@@ -8,32 +8,53 @@ public class Coin : Interactable
 {
     public CanvasGroup fadePanel;
     public TextMeshProUGUI messageText;
-    public string[] messages;
+    public GameObject coin;
+
+    public string[] messages = {
+        "Heavens almighty, is it that hard to understand!? You have to hold your hands like this, you’re doing it too lazily.",
+        "I-I’m sorry dad, I’m trying.",
+        "Clearly you are not, it’s not that difficult! You keep forgetting everything, keep messing up the rituals, keep bringing the wrong ingredients.",
+        "Do you even understand how important this is? How much people are going to depend on you to do this job the right way?",
+        "DO YOU UNDERSTAND WHAT THEY’LL DO TO US IF WE DON’T FEED HIM.",
+        "Dad, calm down, please.",
+        "You notice the very young teenager start to break down in tears, and the person who you assume to be the person’s dad, starts to calm down.",
+        "Stay here, I’m going to fetch something for you.",
+        "The father leaves the fire area, and the kid continues to cry. Soon after though, the kid changes mood as if possessed.",
+        "Filthy fire, this is all your fault! You’re just a dumb fire, why can’t you just go away.",
+        "The kid begins recklessly bashing the books into the flames. The fire, however, consumed them like a beast devoid of food for centuries; hundreds of records lost.",
+        "The kid then turns towards a box of valuable trinkets meant for the rituals. He begins flinging them at the fire with ultimate violence.",
+        "Go away, GO AWAY.",
+        "You see the father rush onto the scene, but it’s too late. The only thing that remains is a coin that the child dropped on the floor amongst the chaos.",
+        "The second offering has been submitted…",
+        "WHAT IN GOD'S NAME ARE YOU DOING? GET AWAY FROM THE FIRE YOU IDIOT.",
+        "The child prepares to slam the now empty box which formerly contained the trinkets into the flames.",
+        "However, the child loses his footing, and falls straight into the flames. The father helplessly watches on as the fire consumed the one thing he loved.",
+        "The fire returns to its normal state, seemingly unaffected by the heaps of objects and violence thrown at it.",
+        "The father stood as still as a charred tree after a wildfire, rooted in place, his eyes hollow and empty, as if the flames had burned away every last trace of feeling.",
+        "The second offering has been accepted…"
+
+    };
+
+    public Color[] messageColors = {
+        Color.white, Color.yellow, Color.white, Color.yellow,
+        Color.yellow, Color.white, Color.yellow, Color.grey,
+        Color.yellow, Color.grey, Color.grey, Color.yellow,
+        Color.grey, Color.grey, Color.white, Color.grey, Color.grey,
+        Color.grey, Color.grey, Color.grey
+    };
+
     public float fadeDuration = 1f;
-    public float messageDisplayTime = 2f;
 
     private bool playerInRange = false;
     public static bool isPickedUp = true;
     public static bool appeared = false;
+    private int count = 0;
+
 
     public override void Interact()
     {
         base.Interact();
         StartCoroutine(PickupSequence());
-    }
-
-    IEnumerator PickupSequence()
-    {
-        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 0, 1, fadeDuration));
-
-        foreach (string message in messages)
-        {
-            yield return StartCoroutine(ShowMessage(message));
-        }
-
-        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 1, 0, fadeDuration));
-
-        gameObject.SetActive(false);
     }
 
     IEnumerator FadeCanvasGroup(CanvasGroup cg, float startAlpha, float endAlpha, float duration)
@@ -51,12 +72,50 @@ public class Coin : Interactable
         cg.blocksRaycasts = endAlpha == 1;
     }
 
+    IEnumerator PickupSequence()
+    {
+        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 0, 1, fadeDuration));
+        coin.SetActive(false);
+        foreach (string message in messages)
+        {
+            yield return ShowMessage(message);
+        }
+
+        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 1, 0, fadeDuration));
+    }
+
     IEnumerator ShowMessage(string message)
     {
         messageText.text = message;
-        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 0, 1, fadeDuration));
-        yield return new WaitForSeconds(messageDisplayTime);
-        yield return StartCoroutine(FadeCanvasGroup(fadePanel, 1, 0, fadeDuration));
+
+        Debug.Log(messageColors[count]);
+        messageText.color = messageColors[count];
+
+        yield return StartCoroutine(FadeTextAlpha(messageText, 0, 1, fadeDuration));
+        yield return new WaitForSeconds(3f);
+        yield return StartCoroutine(FadeTextAlpha(messageText, 1, 0, fadeDuration));
+
+        yield return new WaitForSeconds(0.5f);
+        count++;
+    }
+
+    IEnumerator FadeTextAlpha(TextMeshProUGUI text, float startAlpha, float endAlpha, float duration)
+    {
+        Color color = text.color;
+        color.a = startAlpha;
+        text.color = color;
+
+        float elapsed = 0f;
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+            color.a = Mathf.Lerp(startAlpha, endAlpha, elapsed / duration);
+            text.color = color;
+            yield return null;
+        }
+
+        color.a = endAlpha;
+        text.color = color;
     }
 
 }
